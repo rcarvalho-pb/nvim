bd2b4dc054e38a94a8a50416996a2cf9ec92ffd1
Ramon Carvalho <rcarvalho.pb@gmail.com>
2022-09-23 22:56:46 -0300

    The configuration continues

--- lua/lsp-config/nvim-cmp.lua
+++ lua/lsp-config/nvim-cmp.lua
@@ -1,3 +1,9 @@
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+local lspkind = require('lspkind')
+
+-- nvim-cmp setup
 local cmp = require 'cmp'
 cmp.setup {
   snippet = {
@@ -35,5 +41,18 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+  },
+  formatting = {
+    format = lspkind.cmp_format({
+      with_text = true,
+      --mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
+        return vim_item
+      end
+    })
   },
 }
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -89,6 +89,11 @@ _G.packer_plugins = {
     path = "/home/ramon/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
+  ["lspkind.nvim"] = {
+    loaded = true,
+    path = "/home/ramon/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
+    url = "https://github.com/onsails/lspkind.nvim"
+  },
   ["nightfox.nvim"] = {
     loaded = true,
     path = "/home/ramon/.local/share/nvim/site/pack/packer/start/nightfox.nvim",
